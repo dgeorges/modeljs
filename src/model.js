f95a95c04b929913df4d63b2e6481590143e627e
Daniel Georges <daniel.georges@gmail.com>
2012-08-21 09:10:34 -0700

    added "use strict" and correctly namespaced my library to only expose Model

--- src/model.js
+++ src/model.js
@@ -16,7 +16,6 @@
  * TODO:
  *  - Optimize Transaction callbacks, by being smart/removing duplicates etc...
  *  - hook up/create documentation and document methods properly.
- *  - Add "use strict"
  *  - Think about using ECMA gettter / setters
  *  - clean up unit tests
  *
@@ -25,257 +24,263 @@
  *     to make more sense. Right now its the same as a property change.
  */
 
-/**
- * Centerlized place where all Model Events pass through.
- * @type {Object}
- */
-var eventProxy = function () {
-    var eventQueue = [],
-        state = {   ACTIVE: "active", TRANSACTION: "transaction"},
-        currentState = state.ACTIVE;
+(function (window, undefined) {
+    "use strict";
 
-    function _fireEvent(property, oldValue) {
+    /**
+     * Centerlized place where all Model Events pass through.
+     * @type {Object}
+     */
+    var eventProxy = function () {
+        var eventQueue = [],
+            state = {   ACTIVE: "active", TRANSACTION: "transaction"},
+            currentState = state.ACTIVE;
 
-        // This weird executeCallback function is a bit more complicated than it needs to be but is
-        // used to get around the JSLint warning of creating a function within the while loop below
-        var executeCallbacksFunction = function (oldValue, property) {
-            return function (callback){
-                callback.call(null, oldValue, property.getter_setter());
+        function _fireEvent(property, oldValue) {
+
+            // This weird executeCallback function is a bit more complicated than it needs to be but is
+            // used to get around the JSLint warning of creating a function within the while loop below
+            var executeCallbacksFunction = function (oldValue, property) {
+                return function (callback){
+                    callback.call(null, oldValue, property.getter_setter());
+                };
             };
-        };
 
-        property.getListeners().forEach(
-            executeCallbacksFunction(oldValue, property)
-        );
-
-        var propertyParent = property.getParent();
-        while (propertyParent){
-
-            propertyParent.getListeners(false).forEach(
-                executeCallbacksFunction(oldValue, propertyParent)
+            property.getListeners().forEach(
+                executeCallbacksFunction(oldValue, property)
             );
-            propertyParent = propertyParent.getParent();
-        }
-    }
 
-    function fireEvent (property, oldValue) {
-        if (currentState === state.ACTIVE){
-            _fireEvent(property, oldValue);
-        } else { //place event on queue to be called at a later time.
-            eventQueue.push({
-                property: property,
-                oldValue: oldValue
+            var propertyParent = property.getParent();
+            while (propertyParent){
+
+                propertyParent.getListeners(false).forEach(
+                    executeCallbacksFunction(oldValue, propertyParent)
+                );
+                propertyParent = propertyParent.getParent();
+            }
+        }
+
+        function fireEvent (property, oldValue) {
+            if (currentState === state.ACTIVE){
+                _fireEvent(property, oldValue);
+            } else { //place event on queue to be called at a later time.
+                eventQueue.push({
+                    property: property,
+                    oldValue: oldValue
+                });
+            }
+        }
+
+        function changeState(newState){
+            if (state[newState] !== currentState){
+                currentState = newState;
+                if (newState === state.ACTIVE){
+                    flushEventQueue();
+                }
+            }
+        }
+
+        function flushEventQueue() {
+            eventQueue.forEach( function (event){
+                _fireEvent(event.property, event.oldValue);
             });
+            eventQueue = [];
         }
-    }
 
-    function changeState(newState){
-        if (state[newState] !== currentState){
-            currentState = newState;
-            if (newState === state.ACTIVE){
-                flushEventQueue();
-            }
-        }
-    }
+        return {
+            fireEvent: fireEvent,
+            startTransaction: changeState.bind(null, state.TRANSACTION),
+            endTransaction: changeState.bind(null, state.ACTIVE),
+            inTransaction: function () { return currentState === state.TRANSACTION;}
+        };
+    }();
 
-    function flushEventQueue() {
-        eventQueue.forEach( function (event){
-            _fireEvent(event.property, event.oldValue);
+    /**
+     * A given propery in the model.
+     *
+     * @param {[type]} value   property Value
+     * @param {[type]} options May contain the following:
+     *                         validator - a function to validate the new value is valid before it is assigned.
+     */
+    function Property (parent, value, options) {
+
+        Object.defineProperty(this, "_parent", {
+            value: parent,
+            enumerable: false
         });
-        eventQueue = [];
+
+        Object.defineProperty(this, "_myValue", {
+            value: value,
+            enumerable: false,
+             writable: true
+        });
+
+        Object.defineProperty(this, "_options", {
+            value: options,
+            enumerable: false
+        });
+
+        Object.defineProperty(this, "_propertyListeners", {
+            value: [],
+            enumerable: false
+        });
+         Object.defineProperty(this, "_modelListeners", {
+            value: [],
+            enumerable: false
+        });
+        this.getter_setter(value);
     }
 
-    return {
-        fireEvent: fireEvent,
-        startTransaction: changeState.bind(null, state.TRANSACTION),
-        endTransaction: changeState.bind(null, state.ACTIVE),
-        inTransaction: function () { return currentState === state.TRANSACTION;}
+    /**
+     * [getter_setter description]
+     * @param  {[type]} newValue The Value you want to assign to the Property.
+     * @param  {[type]} options  May contain the following:
+     *                           suppressNotifications - boolean indicating if listeners should be notified of change.
+     *
+     *
+     * @return {[type]}          The resulting value of the Property
+     */
+    Property.prototype.getter_setter = function (newValue, options) {
+
+        if (newValue !== undefined && newValue !== this._myValue) { // Note: this disallows setting a property to undefined. Only when it's first created can it be undefined.
+            var validationFunction = this._options && this._options.validator;
+
+            if (!validationFunction || validationFunction(newValue)){
+                var oldValue = this._myValue;
+                this._myValue = newValue;
+
+                if (!options || !options.suppressNotifications){
+                    eventProxy.fireEvent(this, oldValue);
+                }
+            }
+        }
+        return this._myValue;
     };
-}();
 
-/**
- * A given propery in the model.
- *
- * @param {[type]} value   property Value
- * @param {[type]} options May contain the following:
- *                         validator - a function to validate the new value is valid before it is assigned.
- */
-function Property (parent, value, options) {
-
-    Object.defineProperty(this, "_parent", {
-        value: parent,
-        enumerable: false
-    });
-
-    Object.defineProperty(this, "_myValue", {
-        value: value,
-        enumerable: false,
-         writable: true
-    });
-
-    Object.defineProperty(this, "_options", {
-        value: options,
-        enumerable: false
-    });
-
-    Object.defineProperty(this, "_propertyListeners", {
-        value: [],
-        enumerable: false
-    });
-     Object.defineProperty(this, "_modelListeners", {
-        value: [],
-        enumerable: false
-    });
-    this.getter_setter(value);
-}
-
-/**
- * [getter_setter description]
- * @param  {[type]} newValue The Value you want to assign to the Property.
- * @param  {[type]} options  May contain the following:
- *                           suppressNotifications - boolean indicating if listeners should be notified of change.
- *
- *
- * @return {[type]}          The resulting value of the Property
- */
-Property.prototype.getter_setter = function (newValue, options) {
-
-    if (newValue !== undefined && newValue !== this._myValue) { // Note: this disallows setting a property to undefined. Only when it's first created can it be undefined.
-        var validationFunction = this._options && this._options.validator;
-
-        if (!validationFunction || validationFunction(newValue)){
-            var oldValue = this._myValue;
-            this._myValue = newValue;
-
-            if (!options || !options.suppressNotifications){
-                eventProxy.fireEvent(this, oldValue);
-            }
-        }
-    }
-    return this._myValue;
-};
-
-/**
- * [addChangeCallback description]
- * @param {Function} callback [description]
- * @param {[type]}   options May contain the following:
- *                         listenToChildren - registars the listener with any subproperty change.
- */
-Property.prototype.addChangeCallback = function (callback, options) {
-    if (options && options.listenToChildren){
-        this._modelListeners.push(callback);
-    } else {
-        this._propertyListeners.push(callback);
-    }
-};
-
-Property.prototype.getOptions = function () {
-    return this._options;
-};
-
-
-Property.prototype.getListeners = function (type) { //TODO type is very undiscriptive
-    if (type === undefined){ //return all listeners
-        return this._propertyListeners.concat(this._modelListeners);
-    } else if (type) {
-        return this._propertyListeners;
-    } else {
-        return this._modelListeners;
-    }
-
-};
-
-Property.prototype.getParent = function () {
-    return this._parent;
-};
-
-/**
- * The model Object that wraps the JSON.
- * @param {[type]} json [description]
- */
-function Model (json, parent) {
-    var jsonModel = json || {};
-
-    //A Model is in itself a Property so let inherit property
-    // call with empty options for now and this is the value
-    Property.call(this, parent, this, {});
-
-    Object.defineProperty(this, "onChange", {
-        value: this.addChangeCallback
-    });
-
-    Object.keys(jsonModel).forEach(function (name){
-
-        if (name.match(Model.PROPERTY_OPTIONS_SERIALIZED_NAME_REGEX)){
-            return;
-        }
-
-        var value = jsonModel[name];
-        var options = json[name + Model.PROPERTY_OPTIONS_SERIALIZED_NAME_SUFFIX];
-
-        if (value !== null && typeof value === 'object'){
-            var subModel = new Model(value);
-            this.createProperty(name, subModel, options);
+    /**
+     * [addChangeCallback description]
+     * @param {Function} callback [description]
+     * @param {[type]}   options May contain the following:
+     *                         listenToChildren - registars the listener with any subproperty change.
+     */
+    Property.prototype.addChangeCallback = function (callback, options) {
+        if (options && options.listenToChildren){
+            this._modelListeners.push(callback);
         } else {
-            this.createProperty(name, value, options);
+            this._propertyListeners.push(callback);
+        }
+    };
+
+    Property.prototype.getOptions = function () {
+        return this._options;
+    };
+
+
+    Property.prototype.getListeners = function (type) { //TODO type is very undiscriptive
+        if (type === undefined){ //return all listeners
+            return this._propertyListeners.concat(this._modelListeners);
+        } else if (type) {
+            return this._propertyListeners;
+        } else {
+            return this._modelListeners;
         }
 
-    }, this);
-}
-Model.prototype = Object.create(Property.prototype);
+    };
 
+    Property.prototype.getParent = function () {
+        return this._parent;
+    };
 
-Model.PROPERTY_OPTIONS_SERIALIZED_NAME_SUFFIX = "__modeljs__options";
-Model.PROPERTY_OPTIONS_SERIALIZED_NAME_REGEX = /__modeljs__options$/;
+    /**
+     * The model Object that wraps the JSON.
+     * @param {[type]} json [description]
+     */
+    function Model (json, parent) {
+        var jsonModel = json || {};
 
-/**
- * [createProperty description]
- * @param  {[type]} name    [description]
- * @param  {[type]} value   [description]
- * @param  {[type]} options [description]
- * @return {[type]}         [description]
- */
-Model.prototype.createProperty = function createProperty(name, value, options) {
-    var prop = new Property (this, value, options);
+        //A Model is in itself a Property so let inherit property
+        // call with empty options for now and this is the value
+        Property.call(this, parent, this, {});
 
-    // **hack We must bind our method to prop otherwise 'this' will be the model since we are placing the method on the model.
-    // this is also how we control what is public
-    var returnFunction = prop.getter_setter.bind(prop);
-    returnFunction.onChange = prop.addChangeCallback.bind(prop);
-    returnFunction.options = prop.getOptions(); //TODO: is this mutable? than it needs to change
-    this[name] = returnFunction;
-};
+        Object.defineProperty(this, "onChange", {
+            value: this.addChangeCallback
+        });
 
-/**
- * [toJSON description]
- * @param  {[type]} includeMetaData indicates if model metadata should be included in the returned JSON
- * @return {[type]}                 The json representation of the Model.
- */
-Model.prototype.toJSON = function (includeMetaData) {
-    var json = {};
-    Object.keys(this).forEach( function (name){
-         var value = this[name]();
-         if (value instanceof Model) {
-            json[name] = value.toJSON();
-         } else {
-            json[name] = value;
-            if (includeMetaData && this[name].options){
-                json[name + Model.PROPERTY_OPTIONS_SERIALIZED_NAME_SUFFIX] = this[name].options;
+        Object.keys(jsonModel).forEach(function (name){
+
+            if (name.match(Model.PROPERTY_OPTIONS_SERIALIZED_NAME_REGEX)){
+                return;
             }
 
-         }
-    }, this);
-    return json;
-};
+            var value = jsonModel[name];
+            var options = json[name + Model.PROPERTY_OPTIONS_SERIALIZED_NAME_SUFFIX];
 
-Model.startTransaction = function () {
-    eventProxy.startTransaction();
-};
+            if (value !== null && typeof value === 'object'){
+                var subModel = new Model(value);
+                this.createProperty(name, subModel, options);
+            } else {
+                this.createProperty(name, value, options);
+            }
 
-Model.endTransaction = function () {
-    eventProxy.endTransaction();
-};
+        }, this);
+    }
+    Model.prototype = Object.create(Property.prototype);
 
-Model.inTransaction = function() {
-    eventProxy.inTransaction();
-};
+
+    Model.PROPERTY_OPTIONS_SERIALIZED_NAME_SUFFIX = "__modeljs__options";
+    Model.PROPERTY_OPTIONS_SERIALIZED_NAME_REGEX = /__modeljs__options$/;
+
+    /**
+     * [createProperty description]
+     * @param  {[type]} name    [description]
+     * @param  {[type]} value   [description]
+     * @param  {[type]} options [description]
+     * @return {[type]}         [description]
+     */
+    Model.prototype.createProperty = function createProperty(name, value, options) {
+        var prop = new Property (this, value, options);
+
+        // **hack We must bind our method to prop otherwise 'this' will be the model since we are placing the method on the model.
+        // this is also how we control what is public
+        var returnFunction = prop.getter_setter.bind(prop);
+        returnFunction.onChange = prop.addChangeCallback.bind(prop);
+        returnFunction.options = prop.getOptions(); //TODO: is this mutable? than it needs to change
+        this[name] = returnFunction;
+    };
+
+    /**
+     * [toJSON description]
+     * @param  {[type]} includeMetaData indicates if model metadata should be included in the returned JSON
+     * @return {[type]}                 The json representation of the Model.
+     */
+    Model.prototype.toJSON = function (includeMetaData) {
+        var json = {};
+        Object.keys(this).forEach( function (name){
+             var value = this[name]();
+             if (value instanceof Model) {
+                json[name] = value.toJSON();
+             } else {
+                json[name] = value;
+                if (includeMetaData && this[name].options){
+                    json[name + Model.PROPERTY_OPTIONS_SERIALIZED_NAME_SUFFIX] = this[name].options;
+                }
+
+             }
+        }, this);
+        return json;
+    };
+
+    Model.startTransaction = function () {
+        eventProxy.startTransaction();
+    };
+
+    Model.endTransaction = function () {
+        eventProxy.endTransaction();
+    };
+
+    Model.inTransaction = function() {
+        eventProxy.inTransaction();
+    };
+
+    window.Model = Model;
+}(window));
