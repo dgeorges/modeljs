7f71abc90871bc700b3470f370d52811bdb97d92
Daniel Georges <daniel.georges@gmail.com>
2012-09-06 19:55:43 -0700

    Adding YUIdoc generated documentation.

---
+++ doc/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Model",
+        "Property"
+    ],
+    "modules": [
+        "Model"
+    ],
+    "allModules": [
+        {
+            "displayName": "Model",
+            "name": "Model",
+            "description": "Provides the base Model library."
+        }
+    ]
+} };
+});
