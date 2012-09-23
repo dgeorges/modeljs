a148fe8b6665f35c5aa3713c2f03a9675d7ee7f3
Daniel Georges <daniel.georges@gmail.com>
2012-09-22 21:15:24 -0700

    moving documentation to docs folder instead of doc folder

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Model",
+        "Property"
+    ],
+    "modules": [
+        "Model"
+    ],
+    "allModules": [
+        {
+            "displayName": "Model",
+            "name": "Model",
+            "description": "Model js - A simple javascript library for creating the Model part of a MVC application.\nhttps://github.com/dgeorges/modeljs.git\n\nCopyright 2012, Daniel Georges\nmodeljs is distributed freely under a MIT licence"
+        }
+    ]
+} };
+});
