a86523f31304180938e0bcd990e0c10acce13b37
Daniel Georges <daniel.georges@gmail.com>
2013-10-30 23:47:44 -0700

    fix spacing and add usage comment

--- test/nodeTestRunner.js
+++ test/nodeTestRunner.js
@@ -1,4 +1,5 @@
-    var testrunner = require("qunit");
-    // set up Test runner
-    //testrunner.module("modeljs Tests");
-    testrunner.run({code:"./src/model", tests:"./test/ModelTests.js"});
+// usage: npm -run-script test or node ./test/nodeTestRunner.js
+var testrunner = require("qunit");
+// set up Test runner
+//testrunner.options.coverage = true;
+testrunner.run({code:"./src/model", tests:"./test/ModelTests.js"});
